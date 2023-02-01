812575ad9f660a09d7564167881243b38c8fd0fa
Erkay Aktaş <100722320+ErkayAktas@users.noreply.github.com>
2023-02-01 19:12:41 +0300

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -3,11 +3,11 @@ import time
 abc=['a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n','o', 'ö', 'p', 'r', 's', 'ş', 't', 'u', 'ü','v', 'y', 'z', 'x', 'w', 'q']
 r=input("give a input: \t")
 let=""
+random.shuffle(abc)
 for i in r:
     if i==" ":
         let+=i
         continue
-    random.shuffle(abc)
     for j in abc:
         time.sleep(0.1)
         print(let+j)
